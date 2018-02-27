ac5560c7450d108c25a990ea006fc6fd932e092b
Bitter <janarvottin@gmail.com>
2018-02-27 19:10:31 +0200

    Prepare code for new platform. So far it compiles and seems to execute on the device. SP flashtool reports that it's unable to initialize DRAM. After comparison with original preloader it seems clear that the preloader is missing DRAM calibration data.

---
+++ device/coolpad/mtk6735_65u_l/AndroidProducts.mk
@@ -0,0 +1,1 @@
+PRODUCT_MAKEFILES := $(LOCAL_DIR)/full_mtk6735_65u_l.mk
---
+++ vendor/mediatek/proprietary/custom/mtk6735_65u_l/Android.mk
@@ -0,0 +1,4 @@
+LOCAL_PATH:= $(call my-dir)
+ifeq ($(MTK_PROJECT), mtk6735_65u_l)
+include $(call all-makefiles-under,$(LOCAL_PATH))
+endif
